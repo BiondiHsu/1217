95919d4934560ddc30a444dc85c965781961e787
徐 崇豪 <biondi@mail.cjcu.edu.tw>
2025-12-24 19:10:44 +0800

    Update 1111 New version CVS-DE Testing-Streamlit.py

--- 1111 New version CVS-DE Testing-Streamlit.py
+++ 1111 New version CVS-DE Testing-Streamlit.py
@@ -33,6 +33,11 @@ import streamlit as st
 st.write("STREAMLIT BOOT OK")
 st.stop()
 
+def main():
+    st.title("退休金計畫互動系統 · Retirement Income App")
+    st.write("MAIN CALLED ✅")
+
+    st.write("這裡開始才是真正 UI")
 # ============================
 # 產生帶 Logo 的 QR Code
 # ============================
@@ -696,6 +701,7 @@ st.markdown("""
 """)
 
 main()
+
 
 
 
